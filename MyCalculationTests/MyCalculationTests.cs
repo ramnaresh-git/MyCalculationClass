e1aaa88cd1ef644345874185818d326102140c05
ramnaresh-git <71754198+ramnaresh-git@users.noreply.github.com>
2020-09-24 21:54:50 -0700

    Adding tests

---
+++ MyCalculationTests/MyCalculationTests.cs
@@ -0,0 +1,27 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyCalculationClasses;
+
+namespace MyCalculationTests
+{
+    [TestClass]
+    public class MyCalculationTests
+    {
+       [TestMethod]
+       public void ValidateAdditionTest()
+        {
+            var addClass = new AddClass();
+            var result = addClass.AddTwoVariables(4, 5);
+            Assert.AreEqual(4 + 5, result);
+        }
+
+       [TestMethod]
+       public void ValidateMutiplyTest()
+       {
+         var multiplyClass = new MultiplyClass();
+         var result = multiplyClass.MultiplyTwoVariables(4, 5);
+         Assert.AreEqual(4 * 5, result);
+
+        }
+        
+    }
+}
